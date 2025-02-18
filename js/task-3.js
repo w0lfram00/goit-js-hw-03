8446dee9a50cfca4ffde867e15563bd42ee23868
Mikita <ghjhjr11@gmail.com>
2025-02-18 15:48:28 +0200

    done

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+
+
+function filterArray(numbers = [], value = 0) {
+    const filteredNumbers = [];
+    for (const num of numbers) {
+        if (num > value) filteredNumbers.push(num);
+    }
+    return filteredNumbers;
+}
